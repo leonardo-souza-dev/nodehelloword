fe8535e40a06b2231b8466057ca02ca202a8f98a
lsserra <leonardotreze@gmail.com>
2016-08-23 18:33:18 -0300

    adjusts2

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var http = require("http");
 
+var porta = process.env.PORT || 5000;
+
 //create http server
 http.createServer(function(req,res){   //req = request , res = response
     res.writeHead(200, {"Content-Type":"text/plain"}); // 200 = OK, Declear the type of file
     res.write("Hello Word!");  //print out "Hello World"
     res.end(); // end the app 
-}).listen(8080); // create any port number to view our app
+}).listen(porta); // create any port number to view our app
 
-console.log("The server is running on port 8080"); 
+console.log("The server is running on port " + porta); 
